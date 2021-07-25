334aa8090efcb55b3c3fe838fefffde22072d013
Ajinkyap331 <ajinkyap331@gmail.com>
2021-07-25 17:58:02 +0530

    Added Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Navbar from './Component/Navbar';
 import Mainbody from './Component/Mainbody'
+import Page from './Component/Page';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Mainbody />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/Classroom-Clone/" component={Mainbody} />
+          <Route exact path="/Classroom-Clone/page" component={Page} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
